5cbc9811ca49b176c341efd23110d10aa86dfaf8
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-04-11 01:03:39 -0600

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleToDo(e.target.new_task_description.value);
+  })
+})
+
+
+function handleToDo (todo) {
+  const list = document.createElement('li');
+  list.textContent = todo;
+  const ul = document.querySelector('ul');
+  ul.append(list);
+}
